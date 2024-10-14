135cd228f4ca900b5a94a689578fa4cc30a4ffb9
AleksHadzhiev <aleksandar@d-centralize.nl>
2024-10-14 14:22:13 +0200

    fix: pylint errors

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -1,13 +1,30 @@
+"""Init the dependencies needed to make API calls."""
 from typing import Annotated
+
 
 from fastapi import Header, HTTPException
 
-
 async def get_token_header(x_token: Annotated[str, Header()]):
+    """Get the token header.
+
+    Args:
+        x_token (Annotated[str, Header): _description_
+
+    Raises:
+        HTTPException: _description_
+    """
     if x_token != 'secret-token':
         raise HTTPException(status_code=401, detail='Invalid token')
 
 
 async def get_query_token(token: str):
+    """Get the query token.
+
+    Args:
+        token (str): _description_
+
+    Raises:
+        HTTPException: _description_
+    """
     if token != 'secret-token':
         raise HTTPException(status_code=401, detail='Invalid token')
